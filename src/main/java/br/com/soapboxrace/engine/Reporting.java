cf4e93574aed3c03d808fadfae969dc1eef3264a
Nilzao <nilzao@gmail.com>
2016-04-03 00:02:43 -0400

    missing methods

---
+++ src/main/java/br/com/soapboxrace/engine/Reporting.java
@@ -0,0 +1,17 @@
+package br.com.soapboxrace.engine;
+
+public class Reporting extends Router {
+
+	public String sendMultiplayerConnect() {
+		return "";
+	}
+
+	public String sendHardwareInfo() {
+		return "";
+	}
+
+	public String sendUserSettings() {
+		return "";
+	}
+
+}
