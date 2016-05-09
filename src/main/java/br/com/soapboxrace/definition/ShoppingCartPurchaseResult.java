c6539d7ce7013aa4633bda519533ba0810f12161
berkay2578 <berkaytgy@gmail.com>
2016-05-09 17:39:25 +0300

    Add PurchaseResultStatusInfo

---
+++ src/main/java/br/com/soapboxrace/definition/ShoppingCartPurchaseResult.java
@@ -0,0 +1,16 @@
+package br.com.soapboxrace.definition;
+
+public final class ShoppingCartPurchaseResult {
+	public final String aFail_maxallowed = "Fail_MaxAllowedPurchasesForThisProduct";
+	public final String aFail_insufficientperfinventory = "Fail_InsufficientPerformancePartInventory";
+	public final String aFail_boosttransdisabled = "Fail_BoostTransactionsAreDisabled";
+	public final String aFail_lockedproduct = "Fail_LockedProductNotAccessibleToThisUser";
+	public final String aFail_personalevelerror = "Fail_PersonaNotRightLevel";
+	public final String aFail_itemnotavail = "Fail_ItemNotAvailableStandalone";
+	public final String aFail_invalidperfupgrade = "Fail_InvalidPerformanceUpgrade";
+	public final String aFail_maxstackorrental = "Fail_MaxStackOrRentalLimit";
+	public final String aFail_insufficientcarslots = "Fail_InsufficientCarSlots";
+	public final String aFail_insufficientfunds = "Fail_InsufficientFunds";
+	public final String aFail_invalidbasket = "Fail_InvalidBasket";
+	public final String aSuccess = "Success";
+}
