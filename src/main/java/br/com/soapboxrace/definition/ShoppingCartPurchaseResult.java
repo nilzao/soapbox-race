98942b332051454b5c26eaafb5550d9f16f273c7
berkay2578 <berkaytgy@gmail.com>
2016-05-12 01:45:53 +0300

    Define possible results

--- src/main/java/br/com/soapboxrace/definition/ShoppingCartPurchaseResult.java
+++ src/main/java/br/com/soapboxrace/definition/ShoppingCartPurchaseResult.java
@@ -1,16 +1,16 @@
 package br.com.soapboxrace.definition;
 
-public final class ShoppingCartPurchaseResult {
-	public static String aFail_maxallowed = "Fail_MaxAllowedPurchasesForThisProduct";
-	public static String aFail_insufficientperfinventory = "Fail_InsufficientPerformancePartInventory";
-	public static String aFail_boosttransdisabled = "Fail_BoostTransactionsAreDisabled";
-	public static String aFail_lockedproduct = "Fail_LockedProductNotAccessibleToThisUser";
-	public static String aFail_personalevelerror = "Fail_PersonaNotRightLevel";
-	public static String aFail_itemnotavail = "Fail_ItemNotAvailableStandalone";
-	public static String aFail_invalidperfupgrade = "Fail_InvalidPerformanceUpgrade";
-	public static String aFail_maxstackorrental = "Fail_MaxStackOrRentalLimit";
-	public static String aFail_insufficientcarslots = "Fail_InsufficientCarSlots";
-	public static String aFail_insufficientfunds = "Fail_InsufficientFunds";
-	public static String aFail_invalidbasket = "Fail_InvalidBasket";
-	public static String aSuccess = "Success";
+public interface ShoppingCartPurchaseResult {
+	String aFail_maxallowed = "Fail_MaxAllowedPurchasesForThisProduct";
+	String aFail_insufficientperfinventory = "Fail_InsufficientPerformancePartInventory";
+	String aFail_boosttransdisabled = "Fail_BoostTransactionsAreDisabled";
+	String aFail_lockedproduct = "Fail_LockedProductNotAccessibleToThisUser";
+	String aFail_personalevelerror = "Fail_PersonaNotRightLevel";
+	String aFail_itemnotavail = "Fail_ItemNotAvailableStandalone";
+	String aFail_invalidperfupgrade = "Fail_InvalidPerformanceUpgrade";
+	String aFail_maxstackorrental = "Fail_MaxStackOrRentalLimit";
+	String aFail_insufficientcarslots = "Fail_InsufficientCarSlots";
+	String aFail_insufficientfunds = "Fail_InsufficientFunds";
+	String aFail_invalidbasket = "Fail_InvalidBasket";
+	String aSuccess = "Success";
 }
