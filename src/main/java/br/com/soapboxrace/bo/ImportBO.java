882dc22c1ae09050a2231cc6efef75f53287c768
berkay2578 <berkaytgy@gmail.com>
2016-05-12 01:45:15 +0300

    Fix errors

--- src/main/java/br/com/soapboxrace/bo/ImportBO.java
+++ src/main/java/br/com/soapboxrace/bo/ImportBO.java
@@ -8,15 +8,8 @@ import br.com.soapboxrace.db.ConnectionDB;
 import br.com.soapboxrace.jaxb.EventsPacketType;
 import br.com.soapboxrace.jaxb.EventsType;
 import br.com.soapboxrace.jaxb.util.UnmarshalXML;
-import br.com.soapboxrace.jpa.CustomCarEntity;
-import br.com.soapboxrace.jpa.CustomPaintEntity;
-import br.com.soapboxrace.jpa.CustomVinylEntity;
 import br.com.soapboxrace.jpa.EventDefinitionEntity;
 import br.com.soapboxrace.jpa.OwnedCarEntity;
-import br.com.soapboxrace.jpa.PerformancePartEntity;
-import br.com.soapboxrace.jpa.ProductEntity;
-import br.com.soapboxrace.jpa.SkillModPartEntity;
-import br.com.soapboxrace.jpa.VisualPartEntity;
 
 public class ImportBO {
 
@@ -45,54 +38,9 @@ public class ImportBO {
 					System.out.println("=======================\n\n");
 					OwnedCarEntity ownedCarEntity = (OwnedCarEntity) UnmarshalXML.unMarshal(xmlStr,
 							new OwnedCarEntity());
-					ownedCarEntity.setId(0);
-					ownedCarEntity.setDurability(100);
-
-					ProductEntity productEntity = new ProductEntity();
-					productEntity.setProductId(productIdStr);
-					List<?> find = connectionDB.find(productEntity);
-					if (find.size() > 0) {
-						productEntity = (ProductEntity) find.get(0);
-					}
-					if (productEntity.getId() == 0) {
-						throw new Exception("PRODUTO [" + productIdStr + "] NAO ENCONTRADO");
-					}
-					ownedCarEntity.setProduct(productEntity);
-
-					CustomCarEntity customCarEntity = ownedCarEntity.getCustomCarList().get(0);
-					customCarEntity.setOwnedCar(ownedCarEntity);
-					customCarEntity.setId(0);
-
-					List<CustomPaintEntity> customPaintList = customCarEntity.getCustomPaintList();
-					for (CustomPaintEntity customPaintEntity : customPaintList) {
-						customPaintEntity.setCustomCar(customCarEntity);
-					}
-					customCarEntity.setCustomPaintList(customPaintList);
-
-					List<PerformancePartEntity> performancePartList = customCarEntity.getPerformancePartList();
-					for (PerformancePartEntity performancePartEntity : performancePartList) {
-						performancePartEntity.setCustomCar(customCarEntity);
-					}
-					customCarEntity.setCustomPaintList(customPaintList);
-
-					List<SkillModPartEntity> skillModPartList = customCarEntity.getSkillModPartList();
-					for (SkillModPartEntity skillModPartEntity : skillModPartList) {
-						skillModPartEntity.setCustomCar(customCarEntity);
-					}
-					customCarEntity.setCustomPaintList(customPaintList);
-
-					List<CustomVinylEntity> customVinylList = customCarEntity.getCustomVinylList();
-					for (CustomVinylEntity customVinylEntity : customVinylList) {
-						customVinylEntity.setCustomCar(customCarEntity);
-					}
-					customCarEntity.setCustomPaintList(customPaintList);
-
-					List<VisualPartEntity> visualPartList = customCarEntity.getVisualPartList();
-					for (VisualPartEntity visualPartEntity : visualPartList) {
-						visualPartEntity.setCustomCar(customCarEntity);
-					}
-					customCarEntity.setCustomPaintList(customPaintList);
-
+					ownedCarEntity.setUniqueCarId(0);;
+					ownedCarEntity.setDurability((short)100);
+					
 					connectionDB.merge(ownedCarEntity);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
