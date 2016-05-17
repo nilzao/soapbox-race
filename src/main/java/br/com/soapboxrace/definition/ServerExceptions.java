07e843f8302b42744248113feb7617f5b21e4a39
berkay2578 <berkaytgy@gmail.com>
2016-05-17 17:40:20 +0300

    Git Error -- ignore

--- src/main/java/br/com/soapboxrace/definition/ServerExceptions.java
+++ src/main/java/br/com/soapboxrace/definition/ServerExceptions.java
@@ -1,10 +1,10 @@
 package br.com.soapboxrace.definition;
 
 public interface ServerExceptions {	
-	public class PersonaIdMismatch extends Exception {
+	public class PersonaIdMismatchException extends Exception {
 		private static final long serialVersionUID = 1L;
 
-		public PersonaIdMismatch (Long expected, Long result) {
+		public PersonaIdMismatchException (Long expected, Long result) {
 	        super ("Expected personaId: " + expected + "\r\nGot personaId: " + result);
 	    }
 	}
