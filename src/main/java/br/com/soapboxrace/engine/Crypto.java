0f3896ca982a02f9ca8c785af730b4a27cfafc13
Nilzao <nilzao@gmail.com>
2016-05-17 22:10:23 -0400

    relaycrypto getting from httpsessionVo

--- src/main/java/br/com/soapboxrace/engine/Crypto.java
+++ src/main/java/br/com/soapboxrace/engine/Crypto.java
@@ -17,7 +17,7 @@ public class Crypto extends Router {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("<UdpRelayCryptoTicket>");
 		stringBuilder.append("<CryptoTicket>");
-		stringBuilder.append("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		stringBuilder.append(getHttpSessionVo(getUserId()).getRelayCryptoTicket());
 		stringBuilder.append("</CryptoTicket>\n");
 		stringBuilder.append("<SessionKey>AAAAAAAAAAAAAAAAAAAAAA==</SessionKey>\n");
 		stringBuilder.append("<TicketIv>AAAAAAAAAAAAAAAAAAAAAA==</TicketIv>\n");
