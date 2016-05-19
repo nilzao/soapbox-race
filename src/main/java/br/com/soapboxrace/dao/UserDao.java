ea8a39f3061758f97943c080a5537dad21a647a4
berkay2578 <berkaytgy@gmail.com>
2016-05-19 20:10:43 +0300

    Add UserDao

---
+++ src/main/java/br/com/soapboxrace/dao/UserDao.java
@@ -0,0 +1,31 @@
+package br.com.soapboxrace.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.soapboxrace.db.SoapboxDao;
+import br.com.soapboxrace.jpa.UserEntity;
+
+public class UserDao extends SoapboxDao {
+
+	@Override
+	public UserEntity findById(Long id) {
+		UserEntity entity = (UserEntity) super.findById(UserEntity.class, id);
+		return entity;
+	}
+
+	public UserEntity findByEmail(String email) {
+		EntityManager manager = getManager();
+		TypedQuery<UserEntity> query = manager.createQuery("SELECT obj FROM UserEntity obj WHERE obj.email = :email",
+				UserEntity.class);
+		query.setParameter("email", email);
+		UserEntity user = null;
+		if (!query.getResultList().isEmpty())
+			user = query.getResultList().get(0);
+		return user;
+	}
+
+	public UserEntity save(UserEntity entity) {
+		return (UserEntity) super.save(entity);
+	}
+}
