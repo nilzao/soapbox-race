d62ec27ab04962d739494c3067f367e4ae7935b9
Nilzao <nilzao@gmail.com>
2016-05-24 19:36:35 -0400

    update launcher.sh for linux

--- launcher.sh
+++ launcher.sh
@@ -2,14 +2,18 @@
 EMAIL=$1
 PASSWD=$2
 URL=http://localhost:1337/soapbox-race/
-USER_TOKEN=$(curl -s $URL"nothing/user/authenticateUser?email="$EMAIL"&password="$PASSWD)
-IN=$USER_TOKEN
-ARRIN=(${IN//./ })
-USERID=${ARRIN[0]}
-TOKEN=${ARRIN[1]}
+SRV_RESPONSE=$(curl -s $URL"nothing/user/authenticateUser?email="$EMAIL"&password="$PASSWD)
+IN=$SRV_RESPONSE
+ARRIN=(${IN//>/ })
+USERID=${ARRIN[2]}
+USERID=${USERID//[^0-9]/}
+TOKEN=${ARRIN[4]}
+TOKEN=${TOKEN//[^0-9]/}
 
+echo SRV: $SRV_RESPONSE
 echo USER: $USERID
+echo TOKEN: $TOKEN
+echo
 
-wine explorer /desktop=SOAPBOX$EMAIL,800x600 soapbox.exe \
+wine explorer /desktop="SOAPBOX["$USERID"]"$EMAIL,800x600 soapbox.exe \
 US $URL $TOKEN $USERID &> /dev/null
-
