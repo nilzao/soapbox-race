cfd18ff0c39f97be912c44cbc380fcb6e9473064
Nilzao <nilzao@gmail.com>
2016-06-20 20:21:02 -0400

    cryptoticket to test freeroam

--- src/main/java/br/com/soapboxrace/engine/Crypto.java
+++ src/main/java/br/com/soapboxrace/engine/Crypto.java
@@ -1,11 +1,23 @@
 package br.com.soapboxrace.engine;
 
+import java.nio.ByteBuffer;
+import java.util.Base64;
+
 public class Crypto extends Router {
 
 	public String cryptoticket() {
+		byte[] helloPacket = { 10, 11, 12, 13 };
+		int personaId = getHttpSessionVo(getUserId()).getPersonaId().intValue();
+		ByteBuffer byteBuffer = ByteBuffer.allocate(32);
+		byteBuffer.put(helloPacket);
+		byteBuffer.putInt(personaId);
+		byte[] cryptoTicketBytes = byteBuffer.array();
+		String cryptoTicketBase64 = Base64.getEncoder().encodeToString(cryptoTicketBytes);
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("<ClientServerCryptoTicket>\n");
-		stringBuilder.append("<CryptoTicket>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=</CryptoTicket>\n");
+		stringBuilder.append("<CryptoTicket>");
+		stringBuilder.append(cryptoTicketBase64);
+		stringBuilder.append("</CryptoTicket>\n");
 		stringBuilder.append("<SessionKey>AAAAAAAAAAAAAAAAAAAAAA==</SessionKey>\n");
 		stringBuilder.append("<TicketIv>AAAAAAAAAAAAAAAAAAAAAA==</TicketIv>\n");
 		stringBuilder.append("</ClientServerCryptoTicket>");
