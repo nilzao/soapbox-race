8c6d02efe72063302ef9259de99e78ba56fd52ab
Nilzao <nilzao@gmail.com>
2016-07-10 12:38:25 -0400

    CategoryDao

--- src/main/java/br/com/soapboxrace/bo/CatalogBO.java
+++ src/main/java/br/com/soapboxrace/bo/CatalogBO.java
@@ -2,8 +2,8 @@ package br.com.soapboxrace.bo;
 
 import java.util.List;
 
-import br.com.soapboxrace.dao.db.CategoryDao;
 import br.com.soapboxrace.dao.factory.DaoFactory;
+import br.com.soapboxrace.dao.factory.ICategoryDao;
 import br.com.soapboxrace.dao.factory.IProductDao;
 import br.com.soapboxrace.jaxb.ArrayOfCategoryTrans;
 import br.com.soapboxrace.jaxb.ArrayOfProductTrans;
@@ -12,7 +12,7 @@ import br.com.soapboxrace.jpa.ProductEntity;
 public class CatalogBO {
 
 	private IProductDao productDao = DaoFactory.getProductDao();
-	private CategoryDao categoryDao = new CategoryDao();
+	private ICategoryDao categoryDao = DaoFactory.getCategoryDao();
 
 	public ArrayOfProductTrans productsInCategory(String categoryName, String clientProductType) {
 		List<ProductEntity> products = productDao.findByCategoryNameClientProductType(categoryName, clientProductType);
--- src/main/java/br/com/soapboxrace/dao/factory/ICategoryDao.java
+++ src/main/java/br/com/soapboxrace/dao/factory/ICategoryDao.java
@@ -1,7 +1,15 @@
 package br.com.soapboxrace.dao.factory;
 
 import br.com.soapboxrace.db.ISoapboxDao;
+import br.com.soapboxrace.jaxb.ArrayOfCategoryTrans;
+import br.com.soapboxrace.jpa.CategoryEntity;
 
 public interface ICategoryDao extends ISoapboxDao {
+
+	public CategoryEntity findById(Long id);
+
+	public ArrayOfCategoryTrans getAll();
+
+	public CategoryEntity save(CategoryEntity entity);
 
 }
