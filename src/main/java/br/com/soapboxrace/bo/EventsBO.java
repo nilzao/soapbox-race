f32e2c485cb0cdf650aa77df6522c57aef194b3d
Nilzao <nilzao@gmail.com>
2016-07-10 12:29:31 -0400

    EventDefinitionDao

--- src/main/java/br/com/soapboxrace/bo/EventsBO.java
+++ src/main/java/br/com/soapboxrace/bo/EventsBO.java
@@ -2,14 +2,15 @@ package br.com.soapboxrace.bo;
 
 import java.util.List;
 
-import br.com.soapboxrace.dao.db.EventDefinitionDao;
+import br.com.soapboxrace.dao.factory.DaoFactory;
+import br.com.soapboxrace.dao.factory.IEventDefinitionDao;
 import br.com.soapboxrace.jaxb.EventsPacketType;
 import br.com.soapboxrace.jaxb.EventsType;
 import br.com.soapboxrace.jpa.EventDefinitionEntity;
 
 public class EventsBO {
 
-	private EventDefinitionDao eventDefinitionDao = new EventDefinitionDao();
+	private IEventDefinitionDao eventDefinitionDao = DaoFactory.getEventDefinitionDao();
 
 	public EventsPacketType availableatlevel(Long userId, String securityToken) {
 		EventsPacketType eventsPacketType = new EventsPacketType();
--- src/main/java/br/com/soapboxrace/dao/factory/IEventDefinitionDao.java
+++ src/main/java/br/com/soapboxrace/dao/factory/IEventDefinitionDao.java
@@ -1,7 +1,14 @@
 package br.com.soapboxrace.dao.factory;
 
+import java.util.List;
+
 import br.com.soapboxrace.db.ISoapboxDao;
+import br.com.soapboxrace.jpa.EventDefinitionEntity;
 
 public interface IEventDefinitionDao extends ISoapboxDao {
+
+	public EventDefinitionEntity findById(Long id);
+
+	public List<EventDefinitionEntity> getAll();
 
 }
